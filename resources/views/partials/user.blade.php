354af3822233281c3868b6475672b97c7cdc08f5
Stef van Esch <stef@marshmallow.dev>
2020-10-08 15:14:32 +0200

    Added a flag to Avatar if translatable is available and active

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -1,17 +1,19 @@
-<dropdown-trigger class="h-9 w-8 flex items-center">
-    @isset($user->email)
-        <img
-            src="https://marshmallow.dev/cdn/media/mrmallow-250x250.png"
-            class="mrmallow rounded-full w-8 h-8 mr-3 pr-3"
-        />
-    @endisset
+<dropdown-trigger class="h-9 flex items-center p-relative">
+    <img src="https://marshmallow.dev/cdn/media/mrmallow-250x250.png" class="mrmallow rounded-full w-8 h-8 mr-3"/>
+
+    @if (class_exists('\Marshmallow\Translatable\Models\Language'))
+        @if (!\Marshmallow\Translatable\Models\Language::currentTranslatableIsDefault())
+            <img src="{{ \Marshmallow\Translatable\Models\Language::currentTranslatableModel()->getIcon() }}"
+                 class="current-language-header-icon rounded-full w-8 h-8 mr-3"/>
+        @endif
+    @endif
 
     <span class="text-90">
         {{ $user->name ?? $user->email ?? __('Nova User') }}
     </span>
 </dropdown-trigger>
 
-<dropdown-menu slot="menu" class="mt-1 z-60 " direction="rtl">
+<dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
         <li>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
