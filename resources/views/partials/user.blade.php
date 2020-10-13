2f2272134c2452f32539f30aecd114682d3719f0
Stef van Esch <stef@marshmallow.dev>
2020-10-13 12:29:10 +0200

    Always show language icon

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -2,10 +2,8 @@
     <img src="https://marshmallow.dev/cdn/media/mrmallow-250x250.png" class="mrmallow rounded-full w-8 h-8 mr-3"/>
 
     @if (class_exists('\Marshmallow\Translatable\Models\Language'))
-        @if (!\Marshmallow\Translatable\Models\Language::currentTranslatableIsDefault())
-            <img src="{{ \Marshmallow\Translatable\Models\Language::currentTranslatableModel()->getIcon() }}"
-                 class="current-language-header-icon rounded-full w-8 h-8 mr-3"/>
-        @endif
+        <img src="{{ \Marshmallow\Translatable\Models\Language::currentTranslatableModel()->getIcon() }}"
+             class="current-language-header-icon rounded-full w-8 h-8 mr-3"/>
     @endif
 
     <span class="text-90">
