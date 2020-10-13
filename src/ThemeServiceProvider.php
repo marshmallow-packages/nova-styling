9212fb5cf131e2092f6e0059ff076b625aab9898
LTKort <LTKort@users.noreply.github.com>
2020-10-13 09:42:48 +0000

    Fix styling

--- src/ThemeServiceProvider.php
+++ src/ThemeServiceProvider.php
@@ -2,16 +2,16 @@
 
 namespace Marshmallow\NovaStyling;
 
-use Laravel\Nova\Nova;
-use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
 
 class ThemeServiceProvider extends ServiceProvider
 {
-    const NOVA_VIEWS_PATH   = __DIR__ . '/../resources/views';
-    const CSS_PATH          = __DIR__ . '/../resources/css';
-    const JS_PATH           = __DIR__ . '/../resources/js';
-    const CONFIG_FILE       = __DIR__ . '/../config/nova-styling.php';
+    const NOVA_VIEWS_PATH = __DIR__ . '/../resources/views';
+    const CSS_PATH = __DIR__ . '/../resources/css';
+    const JS_PATH = __DIR__ . '/../resources/js';
+    const CONFIG_FILE = __DIR__ . '/../config/nova-styling.php';
 
     /**
      * Bootstrap any application services.
@@ -25,28 +25,29 @@ class ThemeServiceProvider extends ServiceProvider
             Nova::style('nova-styling',  __DIR__ . '/../resources/css/responsive.css');
             Nova::script('nova-styling', __DIR__ . '/../resources/js/responsive.js');
             Nova::provideToScript([
-                'mmns' => config('nova-styling')
+                'mmns' => config('nova-styling'),
             ]);
         });
 
         // Publishes Config
         $this->publishes([
-            self::CONFIG_FILE       => config_path('nova-styling.php'),
+            self::CONFIG_FILE => config_path('nova-styling.php'),
         ], 'config');
 
         // Views
         $this->publishes([
-            self::NOVA_VIEWS_PATH   => resource_path('views/vendor/nova')
+            self::NOVA_VIEWS_PATH => resource_path('views/vendor/nova'),
         ]);
 
         // Publish Public CSS for login screen
         $this->publishes([
-            self::CSS_PATH          => public_path('vendor/marshmallow/nova-styling'),
+            self::CSS_PATH => public_path('vendor/marshmallow/nova-styling'),
         ], 'public');
 
         // Sets CSS file as asset
         Nova::theme(asset('vendor/marshmallow/nova-styling/marshmallow-theme.css'));
     }
+
     /**
      * Register any application services.
      *
